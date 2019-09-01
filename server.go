d3a80b05f5db3b3924eb7ee07df6ed78bba80dbb
ryomak <ryooomaaa0413@outlook.com>
2019-09-01 15:46:58 +0900

    fix

--- server.go
+++ server.go
@@ -5,12 +5,6 @@ import (
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
-func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-	return events.APIGatewayProxyResponse{
-		StatusCode: 200,
-		Body:       "Hello AWS Lambda nd Netlify",
-	}, nil
-}
 
 func main() {
 	// Make the handler available for Remote Procedure Call by AWS Lambda
