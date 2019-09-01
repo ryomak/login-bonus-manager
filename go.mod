9cad45a90ff54599139814a47078d201bc71cc7e
ryomak <ryooomaaa0413@outlook.com>
2019-09-01 15:54:31 +0900

    fix

--- go.mod
+++ go.mod
@@ -1,9 +1,3 @@
-module github.com/ryomak/linebot-for-money
+module github.com/ryomak/login-bonus-manager
 
 go 1.12
-
-require (
-	github.com/aws/aws-lambda-go v1.13.1
-	github.com/gin-gonic/gin v1.4.0
-	github.com/line/line-bot-sdk-go v6.3.0+incompatible
-)
---
+++ line-bot/src/handler/handler.go
@@ -0,0 +1,31 @@
+package handler
+
+import (
+	"log"
+	"os"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/line/line-bot-sdk-go/linebot"
+	"github.com/ryomak/login-bonus-manager/src/line"
+)
+
+func LineHandler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	myLineRequest, err := line.UnmarshalLineRequest([]byte(request.Body))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	bot, err := linebot.New(
+		os.Getenv("channelSecret"),
+		os.Getenv("channelToken"),
+	)
+	var tmpReplyMessage string
+	tmpReplyMessage = "回答：" + myLineRequest.Events[0].Message.Text
+	if _, err = bot.ReplyMessage(myLineRequest.Events[0].ReplyToken, linebot.NewTextMessage(tmpReplyMessage)).Do(); err != nil {
+		log.Fatal(err)
+	}
+	return events.APIGatewayProxyResponse{
+		StatusCode: 200,
+		Body:       request.Body,
+	}, nil
+}
